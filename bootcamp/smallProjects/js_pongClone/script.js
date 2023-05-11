5fb3528cc1002b1f1565c8f03b14bdba88e12196
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-05-11 14:15:08 -0300

    adding files to be tracked remotely

---
+++ bootcamp/smallProjects/js_pongClone/script.js
@@ -0,0 +1,1 @@
+// Our game runs inside an update loop. This means that every frame we call a function to update the location and logic of all the parts of our game. We will break everything out in our game into different 
